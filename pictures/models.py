6cd0bfcc89693f11072d23f1f9a3b18d293c8109
0724654276 <you@example.com>
2021-11-26 16:11:22 +0300

    create url file in app and add url patterns

--- pictures/models.py
+++ pictures/models.py
@@ -13,14 +13,32 @@ class Image(models.Model):
     def __str__(self):
         return self.name
 
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
-def __str__(self):
+    def __str__(self):
         return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
 
 class Location(models.Model):
     name = models.CharField(max_length=100)
     
-  def __str__(self):
+    def __str__(self):
         return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
