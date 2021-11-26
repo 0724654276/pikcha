badf6857c6ef76d3a456002bf1c93ccb8eecbfa2
0724654276 <you@example.com>
2021-11-26 15:55:13 +0300

    make an image class with variables in models.py

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    location =  models.ForeignKey(Location,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+  
