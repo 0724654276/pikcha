c6dd6b78938a3626f1f7482656554ca7b0326fb7
0724654276 <you@example.com>
2021-11-26 16:16:28 +0300

    create a welcome view function

--- pictures/urls.py
+++ pictures/urls.py
@@ -3,5 +3,14 @@ from .import views
 
 urlpatterns = [
 
-    
+    # url('^$',views.welcome,name = 'welcome'),
+    url(r'^search/', views.search, name='search_results'),
+    # url(r'^image/(\d+)',views.image,name ='image'),
+    # path('category/<str:category_name>',views.image, name="image"),
+    # url(r'^location',views.location,name='location'),
+    # path('home/',getHomePage),
+    url(r'^$',views.welcome,name='index'),
+    # url(r'^search/',views.search,name='search_results'),
+    # url(r'^images/(\d+)',views.image,name='image'),
+    url(r'^location/',views.location,name='location'),
 ]
--- pictures/views.py
+++ pictures/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Location,Category,Image
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+def welcome(request):
+    images = Image.all_images()
+    return render (request,'index.html',{"images":images})
