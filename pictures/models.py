bab15130bc75f64b363eb4f00e6847cf11acbf8e
0724654276 <you@example.com>
2021-11-26 15:59:32 +0300

    create a location class with a variable in models.py

--- pictures/models.py
+++ pictures/models.py
@@ -8,7 +8,12 @@ class Image(models.Model):
     location =  models.ForeignKey(Location,on_delete=models.CASCADE)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
+    image = CloudinaryField('image')
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    
   
